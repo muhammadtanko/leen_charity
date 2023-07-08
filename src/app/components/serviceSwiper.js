fe3d60d8c6d0816149b129c2c8734c5e71711ec3
muhammadtanko <teekay014@gmail.com>
2023-07-08 14:29:38 +0100

    added the carousel

---
+++ src/app/components/serviceSwiper.js
@@ -0,0 +1,43 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react"
+// import Image1 from "../../../public/images/img1.png"
+import Image from "next/image"
+import "swiper/css"
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import './styles.css';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+
+export const ServiceSwiper = () => {
+    return (
+        <div className="mx-[30px] my-[30px] hidden md:block">
+            <Swiper
+                className="mySwiper"
+                navigation={true}
+                autoplay={{
+                    delay:3000,
+                    disableOnInteraction:false
+                }}
+                pagination={{
+                    clickable:true
+                }}
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={20}
+                slidesPerView={1}
+            >
+                <SwiperSlide className="">
+                    <Image src="/images/img142.png" width={1320} height={600} alt="photo" />
+                </SwiperSlide>
+                <SwiperSlide className="">
+                    <Image src="/images/img142.png" width={1320} height={600} alt="photo" />
+                </SwiperSlide>
+                <SwiperSlide className="">
+                    <Image src="/images/img142.png" width={1320} height={600} alt="photo" />
+                </SwiperSlide>
+
+            </Swiper>
+
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import { NavBar } from "./components/NavBar"
 import { Hero } from "./components/Hero";
+import { ServiceSwiper } from "./components/serviceSwiper"
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <NavBar />
       <main>
         <Hero />
+        <ServiceSwiper/>
       </main>
     </>
   )
