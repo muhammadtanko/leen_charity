557b88a47f81b2af96ca55f41dbd86e0ec9ef610
muhammadtanko <teekay014@gmail.com>
2023-07-14 23:53:54 +0100

    minor fixes

---
+++ src/app/services/test.js
@@ -0,0 +1,25 @@
+
+
+const services = [
+
+    {
+        id: 1,
+        icon:"",
+        name: "orphans",
+        image: "",
+        icon: "",
+        text: "",
+        previousProjects: [
+            {
+                name: "Sewing machine and clothes",
+                location: "kaduna",
+                summary:"",
+                DonationReceived:"100000",
+                moneyDonated:"",
+                activeCampaings:"",
+                openedCharity:"",
+                totalCost:""
+            }
+        ]
+    }
+]
